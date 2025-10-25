e95162448cb50ae7718f7563a22df70963cb7fcb
Atraxi-0 <krishnakant9894@gmail.com>
2025-10-25 17:50:58 +0530

    Review model created

---
+++ models/review.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { create } = require('./listing');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    });
+
+    module.exports = mongoose.model('Review', reviewSchema);
